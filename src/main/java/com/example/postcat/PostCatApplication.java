f29cf8b04c855771ee37253f8d21ccab1bf5b51d
Shuanghui Bao <baoshrainbow@126.com>
2024-01-04 14:19:37 +0800

    init repo

---
+++ src/main/java/com/example/postcat/PostCatApplication.java
@@ -0,0 +1,13 @@
+package com.example.postcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postcat/PostCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
